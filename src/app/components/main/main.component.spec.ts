53bba7ce8871ce0965cb30210f717654c9ac8225
kubac <frodo23@protonmail.com>
2026-01-02 21:07:50 +0100

    test: Change wording

--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 
-describe('MainComponent (new-ui)', () => {
+describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
