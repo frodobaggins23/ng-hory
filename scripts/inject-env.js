3e170c459ef3ba96b743bef5dbf3d4ca7204927b
kubac <frodo23@protonmail.com>
2026-01-09 17:20:35 +0100

    fix: update inject env

--- scripts/inject-env.js
+++ scripts/inject-env.js
@@ -1,22 +1,24 @@
 // scripts/inject-env.js
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
-const mapApiKey = process.env.MAP_API_KEY || "";
-const fileServerHost = process.env.FILE_SERVER_HOST || "";
+const mapApiKey = process.env.API_KEY || '';
+const fileServerHost = process.env.FILE_SERVER_HOST || '';
+
+console.log('mapApiKey:', mapApiKey);
 
 const envFiles = [
-  path.join(__dirname, "../src/environments/environment.ts"),
-  path.join(__dirname, "../src/environments/environment.prod.ts"),
+  path.join(__dirname, '../src/environments/environment.ts'),
+  path.join(__dirname, '../src/environments/environment.prod.ts'),
 ];
 
-envFiles.forEach((file) => {
+envFiles.forEach(file => {
   if (fs.existsSync(file)) {
-    let content = fs.readFileSync(file, "utf8");
+    let content = fs.readFileSync(file, 'utf8');
     content = content.replace(/mapApiKey: '.*'/, `mapApiKey: '${mapApiKey}'`);
     content = content.replace(/fileServerHost: '.*'/, `fileServerHost: '${fileServerHost}'`);
-    fs.writeFileSync(file, content, "utf8");
+    fs.writeFileSync(file, content, 'utf8');
     console.log(`Injected secrets into ${file}`);
   }
 });
